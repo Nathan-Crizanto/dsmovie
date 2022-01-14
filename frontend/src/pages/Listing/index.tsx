40f122054cdd3b28b02ee756a0d6b42fdcaa1b60
nathan <nana.crizanto@gmail.com>
2022-01-14 13:25:55 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,11 +2,34 @@ import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
 
-function Listing (){
-    return(
+function Listing() {
+    return (
         <>
-        <Pagination />
-        <MovieCard />
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                </div>
+            </div>
         </>
     );
 }
